82d01a850fc1c87e4c1dbe0518802e540b30daae
Gelsey615 <xig37@pitt.edu>
2017-04-02 20:25:08 -0400

    Update ParentClass.php

--- ParentClass.php
+++ ParentClass.php
@@ -1,24 +1,23 @@
 <?php
-	// This is the file for the parent class
+// This is the file for the parent class
+  class SimpleCalc {
+    public $result;
 
-	class SimpleCalc {
-		public $result;
+    public function __construct() {
+      $this->result=0.0000001;
+    }
 
- 		public function __construct(){
-			$this->result=0.0000001;
- 		}
+    public function add($n1, $n2) {
+      $this->result=$n1+$n2;
+      return $this->result;
+    }
+    public function sub($n1, $n2) {
+      $this->result=$n1-$n2;
+      return $this->result;
+    }
 
- 		public function add($n1, $n2){
-		  $this->result=$n1+$n2;
- 			return $this->result;
- 		}
-		public function sub($n1, $n2){
-			$this->result=$n1-$n2;
- 			return $this->result;
- 		}
-
- 		public function __toString(){
-			echo "<h2>The output is ".$this->result.".</h2>";
-		}
-	}
+    public function __toString() {
+      echo "<h2>The output is ".$this->result.".</h2>";
+    }
+  }
 ?>
