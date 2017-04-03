7b883f2549adb49c9da4cda4a3dd47a95151d28a
Gelsey615 <xig37@pitt.edu>
2017-04-02 20:22:56 -0400

    Update ChildClass.php

--- ChildClass.php
+++ ChildClass.php
@@ -2,7 +2,7 @@
 // this file will extend PArentClass.php
   class ComplexCalc extends SimpleCalc {
     static $results = 0;
-    function __construct(){
+    function __construct() {
       parent::__construct();
     }
 
@@ -10,18 +10,18 @@
       $this->result = ++self::$results;
     }
 
-    function multiply($n1, $n2){
+    function multiply($n1, $n2) {
       $product=0;
-      for($i=1; $i<=$n2; $i++){
+      for($i=1; $i<=$n2; $i++) {
         $product = parent::add($product, $n1);
       }
       $this->result=$product;
       return $this->result;
     }
 
-    function divide($n1, $n2){
+    function divide($n1, $n2) {
       $i=0;
-      while($n1 >= $n2){
+      while($n1 >= $n2) {
         $n1=parent::sub($n1, $n2);
         $i++;
       }
@@ -29,7 +29,7 @@
       return $this->result;
     }
 
-    function __toString(){
+    function __toString() {
       return "<h2>".parent::__toString()."</h2>";
     }
   }
