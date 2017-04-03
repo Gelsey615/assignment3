986208552223abfc2ee0dedf1451d75a6b63cd81
Gelsey615 <xig37@pitt.edu>
2017-04-02 20:15:50 -0400

    Add files via upload

--- ChildClass.php
+++ ChildClass.php
@@ -1,6 +1,37 @@
-<?php 
+<?php
 	// this file will extend PArentClass.php
 
-	class ChildClass extends ParentClass {
-		
+	class ComplexCalc extends SimpleCalc {
+		static $results = 0;
+		function __construct(){
+ 			parent::__construct();
+ 		}
+
+		function __clone() {
+        $this->result = ++self::$results;
+    }
+
+		function multiply($n1, $n2){
+			$product=0;
+ 			for($i=1; $i<=$n2; $i++){
+ 				$product = parent::add($product, $n1);
+ 			}
+			$this->result=$product;
+			return $this->result;
+ 		}
+
+		function divide($n1, $n2){
+			$i=0;
+ 			while($n1 >= $n2){
+				$n1=parent::sub($n1, $n2);
+				$i++;
+			}
+			$this->result=$i;
+			return $this->result;
+		}
+
+		function __toString(){
+			return "<h2>".parent::__toString()."</h2>";
+		}
 	}
+?>
